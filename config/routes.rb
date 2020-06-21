1b1f5724cb21ecd2556ec6397598c00005105d62
EC2 Default User <ec2-user@ip-172-31-6-30.ap-northeast-1.compute.internal>
2020-06-21 05:04:07 +0000

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/home'
+  get 'static_pages/about'
+  root 'static_pages#home'
+  get '/about', to: 'static_pages#about'
 end
