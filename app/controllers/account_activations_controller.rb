e5ddce0906b2559708bb0ef52240235c98809295
EC2 Default User <ec2-user@ip-172-31-6-30.ap-northeast-1.compute.internal>
2020-07-31 11:58:44 +0000

    修正

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,15 +1,28 @@
 class AccountActivationsController < ApplicationController
     
+  # def edit
+  #   user = User.find_by(email: params[:email])
+  #   # 既に有効になっているユーザーを誤って再度有効化しないため
+  #   if user && !user.activated? && user.authenticated?(:activation, params[:id])
+  #     user.activate
+  #     log_in user
+  #     flash[:success] = "Schedule App へようこそ！"
+  #     redirect_to user
+  #   else
+  #     flash[:danger] = "認証に失敗しました"
+  #     redirect_to root_url
+  #   end
+  # end
+  
   def edit
     user = User.find_by(email: params[:email])
-    # 既に有効になっているユーザーを誤って再度有効化しないため
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-      flash[:success] = "Schedile App へようこそ！"
+      flash[:success] = "Lantern Lanternへようこそ！"
       redirect_to user
     else
-      flash[:danger] = "認証に失敗しました"
+      flash[:danger] = "アクティベーションに失敗しました"
       redirect_to root_url
     end
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://pure-cliffs-71321.herokuapp.com/'
+  host = 'https://limitless-everglades-26067.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
