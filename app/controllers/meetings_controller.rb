01de27ba845b147217e0f852cc931f3446b83296
EC2 Default User <ec2-user@ip-172-31-6-30.ap-northeast-1.compute.internal>
2020-08-10 14:56:10 +0000

    修正

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -25,7 +25,8 @@ class MeetingsController < ApplicationController
       @meeting = current_user.meetings.new(meeting_memo)
       if @meeting.save
         flash[:success] = "入力完了"
-        redirect_to meeting_path(@user.id)
+        # redirect_to meeting_path(@user.id)
+        redirect_to meetings_path(current_user)
       else
         flash[:danger] = "入力が失敗しました"
         render "new"
