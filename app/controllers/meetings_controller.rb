c7d33800089b460d3bb8df86e63684106bc205a5
EC2 Default User <ec2-user@ip-172-31-6-30.ap-northeast-1.compute.internal>
2020-08-11 09:57:26 +0000

    修正

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,5 +1,4 @@
 class MeetingsController < ApplicationController
-  # before_action :set_user
     
     def index
       @meetings = current_user.meetings 
@@ -47,10 +46,6 @@ class MeetingsController < ApplicationController
     
     
     private
-    
-    # def set_user
-    #   @user = User.find(params[:id])
-    # end
     
     def meeting_memo
       params.permit(:start_time,:title, :content,:user_id)
