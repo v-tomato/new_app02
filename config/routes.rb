5cb4eee2f9528b144b8cb900b9602d70b2bf4e71
EC2 Default User <ec2-user@ip-172-31-6-30.ap-northeast-1.compute.internal>
2020-08-11 09:38:50 +0000

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,7 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   
-  # resources :users 
-  
-  resources :users do
-    resources :meetings
-  end
+  resources :users 
   
   resources :account_activations, only: [:edit]
   
