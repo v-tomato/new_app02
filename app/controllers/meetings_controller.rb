72afdcef023788725ea9629db377683030d195cd
EC2 Default User <ec2-user@ip-172-31-6-30.ap-northeast-1.compute.internal>
2020-08-14 06:17:28 +0000

    修正

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -9,7 +9,7 @@ class MeetingsController < ApplicationController
     def show
       @user = current_user
       # @meeting = current_user.meetings.find_by(id: params[:id])
-      @meeting = current_user.meetings.find_by(id: current_user.meetings.id)
+      @meeting = current_user.meetings.find_by(id: current_user.meetings_id)
     end
    
     def new
