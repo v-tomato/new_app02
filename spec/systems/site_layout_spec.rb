6087cf4c980e9e24fa469e56cf4b15e7fe805d74
EC2 Default User <ec2-user@ip-172-31-6-30.ap-northeast-1.compute.internal>
2020-09-21 11:37:34 +0000

    rspec-systems-修正

--- spec/systems/site_layout_spec.rb
+++ spec/systems/site_layout_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "SiteLayouts", type: :system do 
+ 
+ let(:user) { create(:user) }
+ 
+ def signup_valid_information
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: 'password'
+    find(".form-submit").click
+    check 'session_remember_me' if [:remember_me] == 1
+  end
   
   describe "home layout" do
      it "contains root link" do
@@ -26,6 +35,70 @@ RSpec.describe "SiteLayouts", type: :system do
      it "contains questions link" do
         visit questions_path
         expect(page).to have_link "Questions", href: questions_path
+        click_on "Questions"
+        visit questions_path
+        expect(page).to have_link "こちら", href: signup_path
+        expect(page).to have_css ".masthead-heading" ".text-uppercase"
      end
    end
+   
+   
+  # ログイン後のレイアウト
+   describe "home layout when User logged in" do
+     it "contains root link" do
+      visit login_path
+      signup_valid_information
+      expect(current_path).to eq user_meetings_path(1)
+      visit root_path
+      expect(page).to have_link "Home", href: root_path
+     end
+     
+     it "contains logout link" do
+      visit login_path
+      signup_valid_information
+      expect(current_path).to eq user_meetings_path(1)
+      visit login_path
+      expect(page).to have_link 'Logout', href: logout_path
+     end
+     
+     it "contains about link" do
+       visit login_path
+       signup_valid_information
+       expect(current_path).to eq user_meetings_path(1)
+       expect(page).to have_link 'About', href: about_path
+     end
+     
+     it "contains contact link" do
+        visit login_path
+        signup_valid_information
+        expect(current_path).to eq user_meetings_path(1)
+        expect(page).to have_link "Contact", href: contact_path
+     end
+     
+     it "contains questions link" do
+        visit login_path
+        signup_valid_information
+        expect(current_path).to eq user_meetings_path(1)
+        expect(page).to have_link "Questions", href: questions_path
+        click_on "Questions"
+        visit questions_path
+        expect(page).to have_link "こちら", href: signup_path
+        expect(page).to have_css ".masthead-heading" ".text-uppercase"
+     end
+     
+     it "contains profile link" do
+       visit login_path
+       signup_valid_information
+       expect(current_path).to eq user_meetings_path(1)
+       expect(page).to have_link "Profile", href: user_path(1)
+     end
+     
+     it "contains calendar link" do
+       visit login_path
+       signup_valid_information
+       expect(current_path).to eq user_meetings_path(1)
+       expect(page).to have_link "Calendar", href: user_meetings_path(1)
+     end
+   end
+  
 end
--- spec/systems/user_spec.rb
+++ spec/systems/user_spec.rb
@@ -10,25 +10,94 @@ RSpec.describe "User", type: :system do
     find(".form-submit").click
     check 'session_remember_me' if [:remember_me] == 1
   end
+  
+  def valid_edit_information
+    fill_in '名前', with: user.name
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: 'password'
+    fill_in 'パスワード(再入力)', with: 'password'
+    find(".form-submit").click
+  end
+  
+  def invalid_edit_information
+    fill_in '名前', with: user.name
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: 'password'
+    fill_in 'パスワード(再入力)', with: ' '
+    find(".form-submit").click
+  end
+  
+  
+  describe "User-Destroy" do
+    it "is valid user-destroy" do
+      visit login_path
+      signup_valid_information
+      expect(current_path).to eq user_meetings_path(1)
+      click_link "Profile"
+      visit user_path(1)
+      expect(page).to have_css ".show-meetings-text"
+      click_on "退会(ユーザー削除)"
+      expect {
+        expect(page.driver.browser.switch_to.alert.text).to eq "削除しますか？"
+        page.driver.browser.switch_to.alert.accept
+        expect(page).to have_content 'ユーザーを削除しました'       
+      }.to change{ User.count }.by(-1)
+      expect(current_path).to eq root_path
+      expect(page).to have_selector ".alert-success"
+    end
+  end
+  
+  describe "User-Show" do
+    it "should be displayed User-Show-page" do
+      visit login_path
+      signup_valid_information
+      expect(current_path).to eq user_meetings_path(1)
+      click_link "Profile"
+      visit user_path(1)
+      expect(current_path).to eq user_path(1)
+      expect(page).to have_css ".show-meetings-text"
+    end
+  end
+  
+  describe "User-Edit" do
+    it "is valid edit" do
+      visit login_path
+      signup_valid_information
+      expect(current_path).to eq user_meetings_path(1)
+      click_link "Profile"
+      visit user_path(1)
+      expect(page).to have_content "ユーザー名"
+      expect(page).to have_content "メールアドレス"
+      click_link "ユーザー編集"
+      visit edit_user_path(1)
+      expect(page).to have_content "プロフィール"
+      expect(page).to have_content "情報変更"
+      valid_edit_information
+      expect(current_path).to eq user_path(1)
+      expect(page).to have_selector ".alert-success", text: "アカウント情報の変更が完了しました"
+      expect(page).to have_content "ユーザー名"
+      expect(page).to have_content "メールアドレス"
+    end
     
-  it "is valid user-destroy" do
-    visit login_path
-    signup_valid_information
-    expect(current_path).to eq user_meetings_path(1)
-    visit user_path(1)
-    expect(page).to have_css ".show-meetings-text"
-    click_on "退会(ユーザー削除)"
-    
-    expect {
-      expect(page.driver.browser.switch_to.alert.text).to eq "削除しますか？"
-      page.driver.browser.switch_to.alert.accept
-      expect(page).to have_content 'ユーザーを削除しました'       
-    }.to change{ User.count }.by(-1)
-    
-    
-    expect(current_path).to eq root_path
-    expect(page).to have_selector ".alert-success"
-    
+    it "is invalid edit" do
+      visit login_path
+      signup_valid_information
+      expect(current_path).to eq user_meetings_path(1)
+      click_link "Profile"
+      visit user_path(1)
+      expect(page).to have_content "ユーザー名"
+      expect(page).to have_content "メールアドレス"
+      click_link "ユーザー編集"
+      visit edit_user_path(1)
+      expect(page).to have_content "プロフィール"
+      expect(page).to have_content "情報変更"
+      invalid_edit_information
+      expect(current_path).to eq user_path(1)
+      expect(page).to have_selector ".alert-danger", text: "アカウント情報の変更が失敗しました"
+      expect(page).to have_content "プロフィール"
+      expect(page).to have_content "情報変更"
+    end
+  
   end
   
 end
